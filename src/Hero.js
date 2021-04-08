0e49180d32fd48bd940967e092ce485d45ca58da
mahasajid <mahasajid1@gmail.com>
2021-04-08 06:44:09 +0500

    navbar2

--- src/Hero.js
+++ src/Hero.js
@@ -1,206 +1,175 @@
 import React from "react";
 import SearchField from "react-search-field";
-
-const AdminAddChildrenProfiles = (props) => {
+import logo from "./HMSlogo.png"
+const RegisterAsSponsor = (props) => {
   const {
+    firstName,
+    lastName,
+    email,
+    dateOfBirth,
+    setEmail,
     handleLogout,
+    setFirstName,
+    setLastName,
+    setDateOfBirth,
+    cnic,
+    setCnic,
+    phoneNumber,
+    setPhoneNumber,
+    address,
+    setAddress,
+    preferredMediumOfCommunication,
+    setPreferredMediumOfCommunication,
+    numberOfSponsoredChildren,
+    setNumberOfSponsoredChildren,
+    paymentMethod,
+    setPaymentMethod,
+    paymentSchedule,
+    setPaymentSchedule,
+    createSponsorshipRequest,
     setRouter,
-    childName,
-    setChildName,
-    childDateOfBirth,
-    setChildDateOfBirth,
-    childGender,
-    setChildGender,
-    childCurrentAddress,
-    setChildCurrentAddress,
-    childGrade,
-    setChildGrade,
-    childContactInformation,
-    setChildContactInformation,
-    childGuardian1Name,
-    setChildGuardian1Name,
-    childGuardian1Relation,
-    setChildGuardian1Relation,
-    childGuardian1Occupation,
-    setChildGuardian1Occupation,
-    childGuardian1CNIC,
-    setChildGuardian1CNIC,
-    childGuardian2Name,
-    setChildGuardian2Name,
-    childGuardian2Relation,
-    setChildGuardian2Relation,
-    childFamilyBackground,
-    setchildFamilyBackground,
+    applicationStatus
   } = props;
 
   return (
     <body>
-      <section>
-        <nav>
-          <h2>Hunehar Management System</h2>
-          <SearchField />
-          <button onClick={handleLogout}>Logout</button>
+      <section className="navbar">
+        <nav className = "navbarContainer_gray">
+         <img src={logo} className="Applogo" alt="logo" />
+          <h2 className= "titletext">Hunehar Management System</h2>
+<<<<<<< Updated upstream
+          <SearchField className ="search"></SearchField >
+         
+          <p className = "smalltext" onClick={handleLogout}><span>Logout</span></p>
         </nav>
-        <section>
-          <h2>CHILDREN PROFILES</h2>
-          <button
-          // Make new onclick Function
-          // onClick={() =>
-          //   applicationStatus
-          //     ? setRouter("registered")
-          //     : setRouter("unregistered")
-          // }
-          >
-            {" "}
-            HOME PAGE{" "}
-          </button>
+        <nav className = "navbarContainer">
+          <p className = "smalltext" onClick={() => applicationStatus ? (setRouter("registered")) : (setRouter("unregistered"))}><span>HOME PAGE</span></p>
+          <h2 className= "titletext">REGISTER AS A SPONSOR</h2>
+        </nav>
+        <section className="register">
+          <div className="registerContainer">
+            {/* <h2 className="titletext">REGISTER AS A SPONSOR</h2> */}
+=======
+          <SearchField className ="search" />
+         
+          <p className = "smalltext" onClick={handleLogout}><span>Logout</span></p>
+        </nav>
+        <section className = "navbarContainer">
+          <button className = "smalltext"onClick={() => applicationStatus ? (setRouter("registered")) : (setRouter("unregistered"))}> HOME PAGE </button>
         </section>
-        <section className="adminChildrenProfiles">
-          <div className="adminChildrenProfilesContainer">
-            <div>
-              <section>
-                <label>Name *</label>
-                <input
-                  type="text"
-                  autoFocus
-                  required
-                  value={childName}
-                  onChange={(e) => setChildName(e.target.value)}
-                ></input>
-              </section>
-              <section>
-                <label>Date of Birth (DD-MM-YYYY) *</label>
-                <input
-                  type="text"
-                  required
-                  value={childDateOfBirth}
-                  onChange={(e) => setChildDateOfBirth(e.target.value)}
-                ></input>
-              </section>
-              <section>
-                <label>Gender *</label>
-                <input
-                  type="text"
-                  required
-                  value={childGender}
-                  onChange={(e) => setChildGender(e.target.value)}
-                ></input>
-              </section>
-              <section>
-                <label>Current Address *</label>
-                <input
-                  type="text"
-                  required
-                  value={childCurrentAddress}
-                  onChange={(e) => setChildCurrentAddress(e.target.value)}
-                ></input>
-              </section>
-              <section>
-                <label>Grade *</label>
-                <input
-                  type="text"
-                  required
-                  value={childGrade}
-                  onChange={(e) => setChildGrade(e.target.value)}
-                ></input>
-              </section>
-              <section>
-                <label>Contact Information *</label>
-                <input
-                  type="text"
-                  required
-                  value={childContactInformation}
-                  onChange={(e) => setChildContactInformation(e.target.value)}
-                ></input>
-              </section>
-              <section>
-                <label>Guardian 1’s Name *</label>
-                <input
-                  type="text"
-                  required
-                  value={childGuardian1Name}
-                  onChange={(e) => setChildGuardian1Name(e.target.value)}
-                ></input>
-              </section>
-              <section>
-                <label>Guardian 1’s Relation *</label>
-                <input
-                  type="text"
-                  required
-                  value={childGuardian1Relation}
-                  onChange={(e) => setChildGuardian1Relation(e.target.value)}
-                ></input>
-              </section>
-              <section>
-                <label>Guardian 1’s Occupation *</label>
-                <input
-                  type="text"
-                  required
-                  value={childGuardian1Occupation}
-                  onChange={(e) => setChildGuardian1Occupation(e.target.value)}
-                ></input>
-              </section>
-              <section>
-                <label>Guardian 1’s CNIC *</label>
-                <input
-                  type="text"
-                  required
-                  value={childGuardian1CNIC}
-                  onChange={(e) => setChildGuardian1CNIC(e.target.value)}
-                ></input>
-              </section>
-              <section>
-                <label>Guardian 2’s Name *</label>
-                <input
-                  type="text"
-                  required
-                  value={childGuardian2Name}
-                  onChange={(e) => setChildGuardian2Name(e.target.value)}
-                ></input>
-              </section>
-              <section>
-                <label>Guardian 2’s Relation *</label>
-                <input
-                  type="text"
-                  required
-                  value={childGuardian2Relation}
-                  onChange={(e) => setChildGuardian2Relation(e.target.value)}
-                ></input>
-              </section>
-              <section>
-                <label>Family Background *</label>
-                <input
-                  type="text"
-                  required
-                  value={childFamilyBackground}
-                  onChange={(e) => setchildFamilyBackground(e.target.value)}
-                ></input>
-              </section>
+        <section className="register">
+          <div className="registerContainer">
+            <h2 className="titletext">REGISTER AS A SPONSOR</h2>
+>>>>>>> Stashed changes
+            <label className="label-left" >First Name *</label>
+            <label className="label-right">
+            <p className="p_i">Last Name *</p></label>
+            <input className="input-left"
+              type="text"
+              autoFocus
+              required
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            ></input>
+            
+            <input
+              className="input-right"
+              type="text"
+              required
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            ></input>
+            <label className="label-left">Email *</label>
+            <label className="label-right">  <p className="p_ii">Date of Birth (DD-MM-YYYY) *</p></label>
+            <input
+            className="input-left"
+              type="text"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+            
+            <input
+              className="input-right"
+              type="text"
+              required
+              value={dateOfBirth}
+              onChange={(e) => setDateOfBirth(e.target.value)}
+            ></input>
+            <label className="label-left">CNIC </label>
+            <label className="label-right"><p className="p_iii">Phone Number *</p></label>
+            <input
+             className="input-left"
+              type="text"
+              required
+              value={cnic}
+              onChange={(e) => setCnic(e.target.value)}
+            ></input>
+            
+            <input
+              className="input-right"
+              type="text"
+              required
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+            ></input>
+            <label className="label-left">Address *</label>
+            <label className="label-right"><p className="p_iv">Preferred Medium of Communication * </p></label>
+            <input className="input-left"
+              type="text"
+              required
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            ></input>
+           
+            <input className="input-right"
+              type="text"
+              required
+              value={preferredMediumOfCommunication}
+              onChange={(e) =>
+                setPreferredMediumOfCommunication(e.target.value)
+              } // make it into drop down menu
+            ></input>
+            <label className="label-left">Number of Sponsored Children *</label>
+            <label className="label-right"> <p className="p_v">Payment Method *</p></label>
+            <input className="input-left"
+              type="counter"
+              required
+              value={numberOfSponsoredChildren}
+              onChange={(e) => setNumberOfSponsoredChildren(e.target.value)}
+            ></input>
+            
+            <input className="input-right"
+              type="text"
+              required
+              value={paymentMethod}
+              onChange={(e) => setPaymentMethod(e.target.value)} // make it into drop down menu
+            ></input>
+            <label className="label-left"> Payment Schedule *</label>
+            <label className="label-right">  <p className="p_iv"></p></label>
+            <input className="input-left"
+              type="text"
+              required
+              value={paymentSchedule}
+              onChange={(e) => setPaymentSchedule(e.target.value)} // make it into drop down menu
+            ></input>
 
-              <div className="btnContainer">
-                <button
-                  // MakeonClick Function onClick={() => setRouter("registered")}
-                  className="button_red"
-                >
-                  ❌ Discard Changes
-                </button>
-              </div>
-              <div className="btnContainer">
-                <button
-                  // MakeonClick Function onClick={() => setRouter("registered")}
-                  className="buttongreen"
-                >
-                  ✅ Add Child
-                </button>
-              </div>
+            <div className="btnContainer">
+              <button onClick={() => createSponsorshipRequest()} className="button_green">
+                Register Me!
+              </button>
             </div>
           </div>
+        </section >
+        <section className = "bottom_bar">
+         <navbar className = "bottombarContainer">
+        <p className = "smalltext" onClick={() => setRouter("contactus")}><span>Contact Us</span></p>
+        <p className = "smalltext"  onClick={() => setRouter("faqs")}><span>FAQs</span></p>
+        </navbar>
         </section>
-        <button onClick={() => setRouter("contactus")}>Contact Us</button>
-        <button onClick={() => setRouter("faqs")}>FAQs</button>
       </section>
     </body>
   );
 };
 
-export default AdminAddChildrenProfiles;
+export default RegisterAsSponsor;
--- src/registerAsSponsor.js
+++ src/registerAsSponsor.js
@@ -34,22 +34,23 @@ const RegisterAsSponsor = (props) => {
   return (
     <body>
       <section className="navbar">
-        <nav className = "navbarContainer_gray">
-         <img src={logo} className="Applogo" alt="logo" />
-          <h2 className= "titletext">Hunehar Management System</h2>
-          <SearchField className ="search" />
-         
-          <p className = "smalltext" onClick={handleLogout}><span>Logout</span></p>
+        <nav className="navbarContainer_gray">
+          <img src={logo} className="Applogo" alt="logo" />
+          <h2 className="titletext">Hunehar Management System</h2>
+          <SearchField className="search"></SearchField >
+
+          <p className="smalltext" onClick={handleLogout}><span>Logout</span></p>
         </nav>
-        <section className = "navbarContainer">
-          <button className = "smalltext"onClick={() => applicationStatus ? (setRouter("registered")) : (setRouter("unregistered"))}> HOME PAGE </button>
-        </section>
+        <nav className="navbarContainer">
+          <p className="smalltext" onClick={() => applicationStatus ? (setRouter("registered")) : (setRouter("unregistered"))}><span>HOME PAGE</span></p>
+          <h2 className="titletext">REGISTER AS A SPONSOR</h2>
+        </nav>
         <section className="register">
           <div className="registerContainer">
-            <h2 className="titletext">REGISTER AS A SPONSOR</h2>
+            {/* <h2 className="titletext">REGISTER AS A SPONSOR</h2> */}
             <label className="label-left" >First Name *</label>
             <label className="label-right">
-            <p className="p_i">Last Name *</p></label>
+              <p className="p_i">Last Name *</p></label>
             <input className="input-left"
               type="text"
               autoFocus
@@ -57,7 +58,7 @@ const RegisterAsSponsor = (props) => {
               value={firstName}
               onChange={(e) => setFirstName(e.target.value)}
             ></input>
-            
+
             <input
               className="input-right"
               type="text"
@@ -68,13 +69,13 @@ const RegisterAsSponsor = (props) => {
             <label className="label-left">Email *</label>
             <label className="label-right">  <p className="p_ii">Date of Birth (DD-MM-YYYY) *</p></label>
             <input
-            className="input-left"
+              className="input-left"
               type="text"
               required
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             ></input>
-            
+
             <input
               className="input-right"
               type="text"
@@ -85,13 +86,13 @@ const RegisterAsSponsor = (props) => {
             <label className="label-left">CNIC </label>
             <label className="label-right"><p className="p_iii">Phone Number *</p></label>
             <input
-             className="input-left"
+              className="input-left"
               type="text"
               required
               value={cnic}
               onChange={(e) => setCnic(e.target.value)}
             ></input>
-            
+
             <input
               className="input-right"
               type="text"
@@ -107,7 +108,7 @@ const RegisterAsSponsor = (props) => {
               value={address}
               onChange={(e) => setAddress(e.target.value)}
             ></input>
-           
+
             <input className="input-right"
               type="text"
               required
@@ -117,21 +118,22 @@ const RegisterAsSponsor = (props) => {
               } // make it into drop down menu
             ></input>
             <label className="label-left">Number of Sponsored Children *</label>
-            <label className="label-right"> <p className="p_iv">Payment Method *</p></label>
+            <label className="label-right"> <p className="p_v">Payment Method *</p></label>
             <input className="input-left"
               type="counter"
               required
               value={numberOfSponsoredChildren}
               onChange={(e) => setNumberOfSponsoredChildren(e.target.value)}
             ></input>
-            
+
             <input className="input-right"
               type="text"
               required
               value={paymentMethod}
               onChange={(e) => setPaymentMethod(e.target.value)} // make it into drop down menu
             ></input>
-            <label className="label-left">Payment Schedule *</label>
+            <label className="label-left"> Payment Schedule *</label>
+            <label className="label-right">  <p className="p_iv"></p></label>
             <input className="input-left"
               type="text"
               required
@@ -146,11 +148,11 @@ const RegisterAsSponsor = (props) => {
             </div>
           </div>
         </section >
-        <section className = "bottom_bar">
-         <navbar className = "bottombarContainer">
-        <p className = "smalltext" onClick={() => setRouter("contactus")}><span>Contact Us</span></p>
-        <p className = "smalltext"  onClick={() => setRouter("faqs")}><span>FAQs</span></p>
-        </navbar>
+        <section className="bottom_bar">
+          <navbar className="bottombarContainer">
+            <p className="smalltext" onClick={() => setRouter("contactus")}><span>Contact Us</span></p>
+            <p className="smalltext" onClick={() => setRouter("faqs")}><span>FAQs</span></p>
+          </navbar>
         </section>
       </section>
     </body>
