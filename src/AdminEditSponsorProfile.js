e977fc063a599cac40422652425741a86d96b875
mahasajid <mahasajid1@gmail.com>
2021-04-11 03:21:16 +0500

    idk at this point more screens

--- src/AdminEditSponsorProfile.js
+++ src/AdminEditSponsorProfile.js
@@ -1,5 +1,8 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import SearchField from "react-search-field";
+import logo from "./HMSlogo.png";
+import { Button, Form } from "react-bootstrap";
+import { useState, useEffect } from "react";
 
 const AdminEditSponsorProfile = (props) => {
   const { sponsorProfile, setRouter, editSponsorProfile, handlelogout } = props;
@@ -31,137 +34,208 @@ const AdminEditSponsorProfile = (props) => {
 
   return (
     <body>
-      <section>
-        <nav>
-          <h2>Hunehar Management System</h2>
-          <SearchField />
-          <button onClick={handlelogout}>Logout</button>
-        </nav>
-        <section>
-          <h2>SPONSOR PROFILES</h2>
-          <button onClick={() => setRouter("home")}> HOME PAGE </button>
-        </section>
-        <section className="adminEditSponsorProfile">
-          <div className="adminEditSponsorProfileContainer">
-            <label>First Name *</label>
-            <input
-              type="text"
-              autoFocus
-              required
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
-            ></input>
-            <label>Last Name *</label>
-            <input
-              type="text"
-              required
-              value={lastName}
-              onChange={(e) => setLastName(e.target.value)}
-            ></input>
-            <label>Email *</label>
-            <input
-              type="text"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            ></input>
-            <label>Date of Birth (DD-MM-YYYY) *</label>
-            <input
-              type="text"
-              required
-              value={dateOfBirth}
-              onChange={(e) => setDateOfBirth(e.target.value)}
-            ></input>
-            <label>CNIC </label>
-            <input
-              type="text"
-              required
-              value={cnic}
-              onChange={(e) => setCnic(e.target.value)}
-            ></input>
-            <label>Phone Number *</label>
-            <input
-              type="text"
-              required
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-            ></input>
-            <label>Address *</label>
-            <input
-              type="text"
-              required
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-            ></input>
-            <label>Preferred Medium of Communication *</label>
-            <input
-              type="text"
-              required
-              value={preferredMediumOfCommunication}
-              onChange={(e) =>
-                setPreferredMediumOfCommunication(e.target.value)
-              } // make it into drop down menu
-            ></input>
-            <label>Number of Sponsored Children *</label>
-            <input
-              type="text"
-              required
-              value={numberOfSponsoredChildren}
-              onChange={(e) => setNumberOfSponsoredChildren(e.target.value)}
-            ></input>
-            <label>Payment Method *</label>
-            <input
-              type="text"
-              required
-              value={paymentMethod}
-              onChange={(e) => setPaymentMethod(e.target.value)} // make it into drop down menu
-            ></input>
-            <label>Payment Schedule *</label>
-            <input
-              type="text"
-              required
-              value={paymentSchedule}
-              onChange={(e) => setPaymentSchedule(e.target.value)} // make it into drop down menu
-            ></input>
+    <section className="navbar">
+      <section className="editMyProfileSponsor">
+        <div className="editMyProfileSponsorContainer">
 
-            <div className="btnContainer">
-              <button
-                onClick={() => {
-                  setRouter("sponsorprofiles");
-                }}
-                className="button_red"
-              >
-                ❌ Discard Changes
-              </button>
-              <button
-                onClick={() => {
-                  setRouter("sponsorprofiles");
-                  editSponsorProfile({
-                    firstName: firstName,
-                    lastName: lastName,
-                    email: email,
-                    dateOfBirth: dateOfBirth,
-                    cnic: cnic,
-                    phoneNumber: phoneNumber,
-                    address: address,
-                    preferredMediumOfCommunication: preferredMediumOfCommunication,
-                    numberOfSponsoredChildren: numberOfSponsoredChildren,
-                    paymentMethod: paymentMethod,
-                    paymentSchedule: paymentSchedule,
-                    applicationStatus: applicationStatus,
-                    howToAssignChildren: howToAssignChildren,
-                    id: id,
-                  });
-                }}
-                className="buttongreen"
-              >
-                ✅ Save Changes
-              </button>
-            </div>
+
+        <div>
+                  <Form>
+                    <Form.Row>
+                      <div class="col-md-6">
+                        <Form.Label className="label-left">
+                          First Name *
+                        </Form.Label>
+                        <Form.Control
+                          type="text"
+                          required
+                          value={firstName}
+                          onChange={(e) => setFirstName(e.target.value)}
+                        ></Form.Control>
+                      </div>
+                      <div class="col-md-6">
+                        <Form.Label className="label-right">
+                          Last Name *
+                        </Form.Label>
+                        <Form.Control
+                          type="text"
+                          required
+                          value={lastName}
+                          onChange={(e) => setLastName(e.target.value)}
+                        ></Form.Control>
+                      </div>
+                    </Form.Row>
+
+                    <Form.Row>
+                      <div class="col-md-6">
+                        <Form.Label className="label-left">Email *</Form.Label>
+                        <Form.Control
+                          type="text"
+                          required
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                        ></Form.Control>
+                      </div>
+                      <div class="col-md-6">
+                        <Form.Label className="label-right">
+                          Date of Birth (DD-MM-YYYY) *
+                        </Form.Label>
+                        <Form.Control
+                          type="text"
+                          required
+                          value={dateOfBirth}
+                          onChange={(e) => setDateOfBirth(e.target.value)}
+                        ></Form.Control>
+                      </div>
+                    </Form.Row>
+
+                    <Form.Row>
+                      <div class="col-md-6">
+                        <Form.Label className="label-left">CNIC</Form.Label>
+                        <Form.Control
+                          type="text"
+                          required
+                          value={cnic}
+                          onChange={(e) => setCnic(e.target.value)}
+                        ></Form.Control>
+                      </div>
+                      <div class="col-md-6">
+                        <Form.Label className="label-right">
+                          Phone Number *
+                        </Form.Label>
+                        <Form.Control
+                          type="text"
+                          required
+                          value={phoneNumber}
+                          onChange={(e) => setPhoneNumber(e.target.value)}
+                        ></Form.Control>
+                      </div>
+                    </Form.Row>
+
+                    <Form.Row>
+                      <div class="col-md-6">
+                        <Form.Label className="label-left">Address</Form.Label>
+                        <Form.Control
+                          type="text"
+                          required
+                          value={address}
+                        ></Form.Control>
+                      </div>
+
+                      <div class="col-md-6">
+                        <Form.Label className="label-left">
+                          Preferred Medium of Communication *
+                        </Form.Label>
+                        <Form.Control
+                          type="text"
+                          required
+                          value={preferredMediumOfCommunication}
+                          onChange={(e) => setPreferredMediumOfCommunication(e.target.value)}
+                        ></Form.Control>
+                      </div>
+                    </Form.Row>
+
+                    <Form.Row>
+                      <div class="col-md-6">
+                        <Form.Label className="label-left">
+                          Number of Sponsored Children
+                        </Form.Label>
+                        <Form.Control
+                          type="text"
+                          required
+                          value={numberOfSponsoredChildren}
+                        ></Form.Control>
+                      </div>
+
+                      <div class="col-md-6">
+                        <Form.Label className="label-left">
+                          Payment Method
+                        </Form.Label>
+                        <Form.Control
+                          type="text"
+                          required
+                          value={paymentMethod}
+                        ></Form.Control>
+                      </div>
+                    </Form.Row>
+
+                    <div class="row">
+                      <div class="col-md-6">
+                        <div
+                         onClick={() => {
+                          setRouter("sponsorprofiles");
+                          editSponsorProfile({
+                            firstName: firstName,
+                            lastName: lastName,
+                            email: email,
+                            dateOfBirth: dateOfBirth,
+                            cnic: cnic,
+                            phoneNumber: phoneNumber,
+                            address: address,
+                            preferredMediumOfCommunication: preferredMediumOfCommunication,
+                            numberOfSponsoredChildren: numberOfSponsoredChildren,
+                            paymentMethod: paymentMethod,
+                            paymentSchedule: paymentSchedule,
+                            applicationStatus: applicationStatus,
+                            howToAssignChildren: howToAssignChildren,
+                            id: id,
+                          });
+                        }}
+                          class="Button"
+                          className="button_green"
+                        >
+                          {" "}
+                          Save Changes
+                        </div>
+                      </div>
+                      <div class="col-md-6">
+                        <div
+                          onClick={() => {
+                            setRouter("sponsorprofiles");
+                          }}
+                          class="Button"
+                          className="button_redd"
+                        >
+                          {" "}
+                          Discard Changes
+                        </div>
+                      </div>
+                    </div>
+                  </Form>
+                </div>
+
+
+           
           </div>
         </section>
-        <button onClick={() => setRouter("admincontactus")}>Contact Us</button>
-        <button onClick={() => setRouter("adminfaqs")}>FAQs</button>
+        <nav className="navbarContainer_gray">
+          <img src={logo} className="Applogo" alt="logo" />
+          <h2 className="titletext">Hunehar Management System</h2>
+          <p className="smalltext" onClick={handlelogout}>
+            <span>Logout</span>
+          </p>
+          <SearchField placeholder="search..." classNames="search" />
+
+          <nav className="navbarContainer">
+            <p className="smalltext" onClick={() => setRouter("home")}>
+              <span>HOME PAGE</span>
+            </p>
+            <h2 className="titletext">SPONSORSHIP PROFILE</h2>
+          </nav>
+        </nav>
+        <section className="bottombar">
+          <navbar className="bottombarContainer">
+            <p
+              className="smalltext"
+              onClick={() => setRouter("admincontactus")}
+            >
+              <span>Contact Us</span>
+            </p>
+            <p className="smalltext" onClick={() => setRouter("adminfaqs")}>
+              <span>FAQs</span>
+            </p>
+          </navbar>
+        </section>
       </section>
     </body>
   );
