4773f78a1e1525505fa78ed6f3d5b3dd8fdc6516
SIMS-B <22100128@lums.edu.pk>
2021-04-11 21:00:54 +0500

    Fixed Withdraw

--- src/ChildrenProfiles.js
+++ src/ChildrenProfiles.js
@@ -1,204 +1,226 @@
 import React from "react";
 import SearchField from "react-search-field";
 import logo from "./HMSlogo.png";
-import {Button, Form} from 'react-bootstrap';
+import { Button, Form } from "react-bootstrap";
 const ChildrenProfiles = (props) => {
   const {
     handleLogout,
     childData,
     setRouter,
     applicationStatus,
-    withdrawchild
+    withdrawchild,
   } = props;
 
-  console.log(childData)
+  console.log(childData);
   return (
     <body>
-    <section className="navbar">
-
-        <section>
-        </section>
+      <section className="navbar">
+        <section></section>
 
         <section className="childrenProfiles">
-        <div className="childrenProfilesContainer">
-        {childData.map((con, i) => {
-          
-          return (
-         
-              <Form>
-              <Form.Row >
-              <div class = "col-md-6">
-            <Form.Label className= "label-left">Name</Form.Label>
-            <Form.Control
-              type="text"
-              autoFocus
-              required
-              value={childData[i].name}
-            ></Form.Control>
-            </div >
-            <div class = "col-md-6">
-            <Form.Label className= "label-right">Date of Birth (DD-MM-YYYY)</Form.Label>
-            <Form.Control
-              type="text"
-              required
-              value={childData[i].dateOfBirth}
-            ></Form.Control>
-              </div>
-              </Form.Row>
-              
-              <Form.Row >
-              <div class = "col-md-6">
-            <Form.Label className= "label-left">Gender</Form.Label>
-            <Form.Control
-              type="text"
-              autoFocus
-              required
-              value={childData[i].gender}
-            ></Form.Control>
-            </div >
-            <div class = "col-md-6">
-            <Form.Label className= "label-right">Current Address</Form.Label>
-            <Form.Control
-              type="text"
-              value={childData[i].currentAddress}
-            ></Form.Control>
-              </div>
-              </Form.Row>
-             
-              <Form.Row >
-              <div class = "col-md-6">
-            <Form.Label className= "label-left">Grade</Form.Label>
-            <Form.Control
-              type="text"
-              autoFocus
-              required
-              value={childData[i].grade}
-            ></Form.Control>
-            </div >
-            <div class = "col-md-6">
-            <Form.Label className= "label-right">Contact Information</Form.Label>
-            <Form.Control
-              type="text"
-              required
-              value={childData[i].contactInformation}
-            ></Form.Control>
-              </div>
-              </Form.Row>
+          <div className="childrenProfilesContainer">
+            {childData.map((con, i) => {
+              return (
+                <Form>
+                  <Form.Row>
+                    <div class="col-md-6">
+                      <Form.Label className="label-left">Name</Form.Label>
+                      <Form.Control
+                        type="text"
+                        autoFocus
+                        required
+                        value={childData[i].name}
+                      ></Form.Control>
+                    </div>
+                    <div class="col-md-6">
+                      <Form.Label className="label-right">
+                        Date of Birth (DD-MM-YYYY)
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        required
+                        value={childData[i].dateOfBirth}
+                      ></Form.Control>
+                    </div>
+                  </Form.Row>
 
-              <Form.Row >
-              <div class = "col-md-6">
-            <Form.Label className= "label-left">Guardian 1’s Name</Form.Label>
-            <Form.Control
-              type="text"
-              autoFocus
-              required
-              value={childData[i].guardian1Name}
-            ></Form.Control>
-            </div >
-            <div class = "col-md-6">
-            <Form.Label className= "label-right">Guardian 1’s Relation</Form.Label>
-            <Form.Control
-              type="text"
-              required
-              value={childData[i].guardian1Relation}
-            ></Form.Control>
-              </div>
-              </Form.Row>
+                  <Form.Row>
+                    <div class="col-md-6">
+                      <Form.Label className="label-left">Gender</Form.Label>
+                      <Form.Control
+                        type="text"
+                        autoFocus
+                        required
+                        value={childData[i].gender}
+                      ></Form.Control>
+                    </div>
+                    <div class="col-md-6">
+                      <Form.Label className="label-right">
+                        Current Address
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        value={childData[i].currentAddress}
+                      ></Form.Control>
+                    </div>
+                  </Form.Row>
 
-              <Form.Row >
-              <div class = "col-md-6">
-            <Form.Label className= "label-left">Guardian 1’s Occupation</Form.Label>
-            <Form.Control
-              type="text"
-              autoFocus
-              required
-              value={childData[i].guardian1Occupation}
-            ></Form.Control>
-            </div >
-            <div class = "col-md-6">
-            <Form.Label className= "label-right">Guardian 1’s CNIC</Form.Label>
-            <Form.Control
-              type="text"
-              required
-              value={childData[i].guardian1Cnic}
-            ></Form.Control>
-              </div>
-              </Form.Row>
+                  <Form.Row>
+                    <div class="col-md-6">
+                      <Form.Label className="label-left">Grade</Form.Label>
+                      <Form.Control
+                        type="text"
+                        autoFocus
+                        required
+                        value={childData[i].grade}
+                      ></Form.Control>
+                    </div>
+                    <div class="col-md-6">
+                      <Form.Label className="label-right">
+                        Contact Information
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        required
+                        value={childData[i].contactInformation}
+                      ></Form.Control>
+                    </div>
+                  </Form.Row>
 
+                  <Form.Row>
+                    <div class="col-md-6">
+                      <Form.Label className="label-left">
+                        Guardian 1’s Name
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        autoFocus
+                        required
+                        value={childData[i].guardian1Name}
+                      ></Form.Control>
+                    </div>
+                    <div class="col-md-6">
+                      <Form.Label className="label-right">
+                        Guardian 1’s Relation
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        required
+                        value={childData[i].guardian1Relation}
+                      ></Form.Control>
+                    </div>
+                  </Form.Row>
 
-              <Form.Row >
-              <div class = "col-md-6">
-            <Form.Label className= "label-left">Guardian 2’s Name</Form.Label>
-            <Form.Control
-              type="text"
-              autoFocus
-              required
-              value={childData[i].guardian2Name}
-            ></Form.Control>
-            </div >
-            <div class = "col-md-6">
-            <Form.Label className= "label-right">Guardian 2’s Relation</Form.Label>
-            <Form.Control
-              type="text"
-              required
-              value={childData[i].guardian2Relation}
-            ></Form.Control>
-              </div>
-              </Form.Row>
+                  <Form.Row>
+                    <div class="col-md-6">
+                      <Form.Label className="label-left">
+                        Guardian 1’s Occupation
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        autoFocus
+                        required
+                        value={childData[i].guardian1Occupation}
+                      ></Form.Control>
+                    </div>
+                    <div class="col-md-6">
+                      <Form.Label className="label-right">
+                        Guardian 1’s CNIC
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        required
+                        value={childData[i].guardian1Cnic}
+                      ></Form.Control>
+                    </div>
+                  </Form.Row>
 
+                  <Form.Row>
+                    <div class="col-md-6">
+                      <Form.Label className="label-left">
+                        Guardian 2’s Name
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        autoFocus
+                        required
+                        value={childData[i].guardian2Name}
+                      ></Form.Control>
+                    </div>
+                    <div class="col-md-6">
+                      <Form.Label className="label-right">
+                        Guardian 2’s Relation
+                      </Form.Label>
+                      <Form.Control
+                        type="text"
+                        required
+                        value={childData[i].guardian2Relation}
+                      ></Form.Control>
+                    </div>
+                  </Form.Row>
 
-              
-              <Form.Row >
-              <div class = "col">
-            <Form.Label>Family Background</Form.Label>
-            <Form.Control
-              type="text"
-              autoFocus
-              required
-              value={childData[i].familyBackground}
-            ></Form.Control>
-            </div >
+                  <Form.Row>
+                    <div class="col">
+                      <Form.Label>Family Background</Form.Label>
+                      <Form.Control
+                        type="text"
+                        autoFocus
+                        required
+                        value={childData[i].familyBackground}
+                      ></Form.Control>
+                    </div>
+                  </Form.Row>
 
-              </Form.Row>
-
-
-              <div className="btnContainer">
-                <button
-                  onClick={() => withdrawchild(childData[i].id)}
-                  className="button_redd"
-                >
-                  Withdraw Sponsorship for This Child
-                </button>
-              </div>
-            </Form>
-          
-          
-          );
-         
-        })}
-        </div>
-        
-          
+                  <div className="btnContainer">
+                    <button
+                      onClick={() => {
+                        withdrawchild(childData[i].id);
+                        applicationStatus
+                          ? setRouter("registered")
+                          : setRouter("unregistered");
+                      }}
+                      className="button_redd"
+                    >
+                      Withdraw Sponsorship for This Child
+                    </button>
+                  </div>
+                </Form>
+              );
+            })}
+          </div>
         </section>
         <nav className="navbarContainer_gray">
           <img src={logo} className="Applogo" alt="logo" />
-          < h2 className="titletext">Hunehar Management System</h2>
-          <p className="smalltext" onClick={handleLogout}><span>Logout</span></p>
-          <SearchField placeholder ="search..."
-          classNames="search"/> 
-                <nav className="navbarContainer">
-          <p className="smalltext" onClick={() => applicationStatus ? (setRouter("registered")) : (setRouter("unregistered"))}><span>HOME PAGE</span></p>
-          <h2 className="titletext">CHILDREN PROFILES</h2>
-        </nav>
-          
+          <h2 className="titletext">Hunehar Management System</h2>
+          <p className="smalltext" onClick={handleLogout}>
+            <span>Logout</span>
+          </p>
+          <SearchField placeholder="search..." classNames="search" />
+          <nav className="navbarContainer">
+            <p
+              className="smalltext"
+              onClick={() =>
+                applicationStatus
+                  ? setRouter("registered")
+                  : setRouter("unregistered")
+              }
+            >
+              <span>HOME PAGE</span>
+            </p>
+            <h2 className="titletext">CHILDREN PROFILES</h2>
+          </nav>
         </nav>
         <section className="bottombar">
           <navbar className="bottombarContainer">
-            <p className="smalltext" onClick={() => setRouter("contactus")}><span>Contact Us</span></p>
-            <p className="smalltext" onClick={() => setRouter("faqs")}><span>FAQs</span></p>
+            <p className="smalltext" onClick={() => setRouter("contactus")}>
+              <span>Contact Us</span>
+            </p>
+            <p className="smalltext" onClick={() => setRouter("faqs")}>
+              <span>FAQs</span>
+            </p>
           </navbar>
         </section>
-        </section>
+      </section>
     </body>
   );
 };
